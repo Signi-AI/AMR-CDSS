844489b30a8232473b871bd8c3c991c95e97d333
mtundudev <dottomtundu9@gmail.com>
2026-09-16 16:03:35 +0300

    remove some router import register

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,7 @@ from fastapi.staticfiles import StaticFiles
 import os
 from app.core import seed
 from app.auth import loginroute
-
-
-from app.api.v1.routers import patient,clinical_visit,pathogen,antimicrobial,result_analyis,cell_cont,clinical_rule
+from app.api.v1.routers import patient,antimicrobial,result_analyis
 from app.api.v1.routers import user
 
 app=FastAPI(title="Clinical Decision Support System Backend")
@@ -13,12 +11,8 @@ app=FastAPI(title="Clinical Decision Support System Backend")
 app.include_router(loginroute.router)
 app.include_router(user.router)
 app.include_router(patient.router)
-app.include_router(clinical_visit.router)
 app.include_router(result_analyis.router)
-app.include_router(pathogen.router)
 app.include_router(antimicrobial.router)
-app.include_router(cell_cont.router)
-app.include_router(clinical_rule.router)
 app.include_router(seed.router)
 
 
